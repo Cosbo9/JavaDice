170361bd233f18cf983db5b5458391e36d4f527c
sirSlappz <tamestranger43@gmail.com>
2022-10-19 11:41:39 -0500

    broke down code from OOP to get it working

--- Main.java
+++ Main.java
@@ -6,31 +6,79 @@ public class Main {
     DieHandler handler = new DieHandler();
     
     public Main(){
-        // trying to inizalize the options chosen by user
-        handler.setDieAmount(amount);
-        handler.setDieType(type); 
     }
+    public static void diceAmount() {
+        // need to capture this input to send to the calculator component
+        // Scanner dieAmount = new Scanner(System.in);
+        // int amount = dieAmount.nextInt();
+    
+        // System.out.println("You have chosen to roll " + amount + " dice");
+        
+    }
+
+    public static void diceType() {
+        // need to capture this input to send to the calculator component
+        System.out.println("Please type in which die you would like to roll: (select only 1 type)\n'd6'\n'd20'");
+        Scanner dieType = new Scanner(System.in);
+        String type = dieType.nextLine();
+
+        switch (type.toLowerCase()) {
+            case "d6":
+                System.out.println("You have selected 'd6'");
+                break;
+            case "d20":
+                System.out.println("You have selected 'd20'");
+                break;
+            default:
+                System.out.println("Invalid selection");
+                diceType();
+                break;
+        }
+    }
+
+    
     public static void main(String[] args) {
         Intro start = new Intro();
         DieHandler handler = new DieHandler();
         Confirmer conf = new Confirmer();
-        Main setterObj = new Main();
-        String type = setterObj.type;
-        int amount = setterObj.amount;
+        // Main setterObj = new Main();
+        // String type = setterObj.type;
+        // int amount = setterObj.amount;
+
+        
 
         start.intro();
-        handler.diceAmount();
-        // experiment trying to see if I can get the variable through this method
-        handler.setDieAmount(amount);
+        
+        Scanner dieAmount = new Scanner(System.in);
+        int amount = dieAmount.nextInt();
+    
+        System.out.println("You have chosen to roll " + amount + " dice");
+        // diceAmount();
         conf.confirm();
-        handler.diceType();
-        // experiment trying to see if I can get the variable through this method
-        handler.setDieType(type); 
+        System.out.println("Please type in which die you would like to roll: (select only 1 type)\n'd6'\n'd20'");
+        Scanner dieType = new Scanner(System.in);
+        String type = dieType.nextLine();
+
+        switch (type.toLowerCase()) {
+            case "d6":
+                System.out.println("You have selected 'd6'");
+                break;
+            case "d20":
+                System.out.println("You have selected 'd20'");
+                break;
+            default:
+                System.out.println("Invalid selection");
+                diceType();
+                break;
+        }
+        // diceType();
         conf.confirm();
-        System.out.println("in main " + type + amount);
+        System.out.println("in main " + amount + " " + type);
         handler.calculator(amount, type);
-        System.out.println("post");
+        conf.redo();
     }
+
+    
 
 
     
@@ -52,6 +100,7 @@ class Intro {
     }
 }
 
+// class that handles UI stuff
 class Confirmer {
     // confirmation component
     public void confirm() {
@@ -97,45 +146,14 @@ class Confirmer {
     }
 }
 
+// class thats going to handle all die options
 class DieHandler {
     int dieAmount;
     String dieType;
-
-    public void diceAmount() {
-        // need to capture this input to send to the calculator component
-        Scanner dieAmount = new Scanner(System.in);
-        int amount = dieAmount.nextInt();
-    
-        System.out.println("You have chosen to roll " + amount + " dice");
-        
-    }
-
-    public void diceType() {
-        // need to capture this input to send to the calculator component
-        System.out.println("Please type in which die you would like to roll: (select only 1 type)\n'd6'\n'd20'");
-        Scanner dieType = new Scanner(System.in);
-        String type = dieType.nextLine();
-
-        switch (type.toLowerCase()) {
-            case "d6":
-                System.out.println("You have selected 'd6'");
-                break;
-            case "d20":
-                System.out.println("You have selected 'd20'");
-                break;
-            default:
-                System.out.println("Invalid selection");
-                diceType();
-                break;
-        }
-    }
-
-    // calculates die rolls, still needs to actually put in the Random functions.
-    // still trying to pass in variable info
-    public int calculator(int diceAmount, String dieType) {
+    // calculates die rolls
+    public void calculator(Integer diceAmount, String dieType) {
+        System.out.println("You have chosen to roll " + diceAmount + " " + dieType);
         int dieValue = 0;
-
-        System.out.println("You have chosen to roll " + dieAmount + " " + dieType);
 
         switch (dieType) {
             case "d6":
@@ -147,22 +165,11 @@ class DieHandler {
             default:
                 break;
         }
-        int total = dieAmount * dieValue;
-        return total;
-    }
-
-
-    // getters and setters in the attempt to inizalize the options user selected
-    public int getDieAmount() {
-        return dieAmount;
-    }
-    public int setDieAmount(int amount) {
-        return this.dieAmount = amount;
-    }
-    public String getDieType() {
-        return dieType;
-    }
-    public String setDieType(String type) {
-        return this.dieType = type;
+        // loop that iterates however many times that user chooses
+        for (int i = 0; i < diceAmount; i++) {
+            // creates and prints random number between 1 and value of chosen die
+            int random = (int) (dieValue * Math.random());
+            System.out.println("You rolled: " + random);
+        } 
     }
 }
